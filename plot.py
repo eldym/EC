975863d7162de22c309628da4ac12920b20c8c56
eldym <eldymoddys@gmail.com>
2025-01-21 13:16:54 -0500

    Removed numpy import, and return in makePlot that just returned a NoneType since difficulties_plot doesn't return anything Added comments

--- plot.py
+++ plot.py
@@ -1,32 +1,43 @@
 import matplotlib.pyplot as plt
-import numpy as np
 from data import *
 
 def difficulties_plot(difficulties, beginIndex):
+    # Creates x axis tickers
     pos = list(range(len(difficulties)))
     newTickers = list(range(beginIndex, beginIndex+len(difficulties)))
 
+    # Plots the data of difficulties
     plt.plot(difficulties)
+
+    # Creates custom tickers that properly display the block numbers
     plt.xticks(pos, newTickers, rotation=90)
+
+    # Labels
     plt.ylabel('Difficulty')
     plt.xlabel('Block #')
+
+    # Saves plotted chart as a png file
     plt.savefig('chart.png', bbox_inches='tight')
-    plt.close()
+    plt.close() # Resets plt
 
 def makePlot():
+    # Gets block data to make a plot
     allBlocks = ecDataGet.getAllBlocks()
     difficulties = []
 
+    # Get beginning index
     if len(allBlocks) > 31:
         i = len(allBlocks) - 31
     else: i = 0
 
     beginIndex = i
     
+    # Appends appropriate data points
     while i < len(allBlocks):
         difficulties.append(allBlocks[i][2])
         i += 1
 
-    return difficulties_plot(difficulties, beginIndex)
+    # Sends data points to make chart image file
+    difficulties_plot(difficulties, beginIndex)
 
-makePlot()
+makePlot() # For running in a dedicated terminal

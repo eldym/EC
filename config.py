4990ec617595b9f08bb3c76a450609c93a5e5efb
eldym <eldymoddys@gmail.com>
2025-01-22 01:09:41 -0500

    Config updates

--- config.py
+++ config.py
@@ -1,16 +1,22 @@
 # Bot config
 PREFIXES = ['!', 'ec!'] # Your prefixes
-DEFAULT_PREFIX = PREFIXES[0] # Default/fallback prefix
-CURRENCY = 'EC <:ec:1220995523469643786>' # Currency
 COOLDOWN = 2 # Cooldown in seconds
-EMB_COLOUR = 0x000000 # Embed colour
-OUTPUT_CHANNEL = None # Enter a channel ID to output completed block info here
+DEFAULT_PREFIX = PREFIXES[0] # Default/fallback prefix
 TOKEN = '' # Your bot's token here
+
+# Currency config
+CURRENCY = 'EC' # Your currency symbol
+CURRENCY_EMOJI = '<:ec:1220995523469643786>' # Optional currency emoji
+DISPLAY_CURRENCY = f'{CURRENCY}{' '+CURRENCY_EMOJI if len(CURRENCY_EMOJI) > 0 else ''}' # Used to display currency + emoji in embeds in main
+
+# Embed config
+EMB_COLOUR = 0x000000 # Embed colour
 EC_THUMBNAIL_LINK = 'https://cdn.discordapp.com/attachments/791182073263685672/1323407110528172143/ec_spin.gif?ex=6774666a&is=677314ea&hm=c6e248a36175fcca642e241488ae733245f898c09f6ed22c2360c7045a474e6a' # Picture that shows on top right of embed on transaction info/mining/etc.
 ERROR_TXT = 'Error!' # Error text
+OUTPUT_CHANNEL = None # Enter a channel ID to output completed block info here
 
 # Admin config
-ADMIN_ID = None # Your Discord User ID to use Admin commands
+ADMIN_ID = None # Discord User ID to use Admin commands
 
 # DB config
 HB_HOST = 'localhost' # Your host

2bf28e44c3abebfbc47b900b267e9d09ec992496
eldym <eldymoddys@gmail.com>
2025-02-04 01:09:59 -0500

    Added getter for autominer

--- database/__init__.py
+++ database/__init__.py
@@ -13,7 +13,7 @@ class Database():
     def __init__(self):
         self.db = mysql.connector.connect(host=config["db_host"],user=config["db_user"],passwd=config["db_pass"],database=config["db_name"])
     
-    def getUser(self, uuid):
+    def get_user(self, uuid):
         # Get specific user's data
         cursor = self.db.cursor()
         cursor.execute(f"SELECT * FROM users WHERE uuid = {uuid}")
@@ -21,3 +21,12 @@ class Database():
         user = None
         for user in cursor: pass
         return user
+    
+    def get_auto_miner(self, uuid):
+        # Gets automining users
+        cursor = self.db.cursor()
+        cursor.execute(f"SELECT * FROM automining_data WHERE miner_id = {uuid}")
+
+        autominer = None
+        for autominer in cursor: pass
+        return autominer

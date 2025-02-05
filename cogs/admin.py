90592b861ea6cc8facd48267de2c0868a167acf0
eldym <eldymoddys@gmail.com>
2025-02-04 23:00:46 -0500

    Admin commands uncommented, modified to conform to cogs

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,7 +1,16 @@
 import discord
 import asyncio
+import json
 
 from discord.ext import commands
+
+def get_config():
+    with open('config.json') as f:
+        return json.load(f)
+
+config = get_config()
+
+DISPLAY_CURRENCY = config["display_currency"]
 
 class Admin(commands.Cog):
     """
@@ -15,31 +24,30 @@ class Admin(commands.Cog):
     async def addBal(self, ctx, uuid, amount):
         # Adds balance to a specific user
         if ctx.author.id == self.bot.owner_id:
-            print("ab run")
-            """uuid = ''.join(uuid).strip('<@>')
-            ecDataManip.updateUserBal(uuid, float(ecDataGet.getUser(uuid)[1]) + float(amount))
-            await ctx.reply(f'Updated user funds by: {amount} {DISPLAY_CURRENCY}')"""
+            uuid = ''.join(uuid).strip('<@>')
+            self.bot.database.update_user_bal(uuid, float(self.bot.database.get_user(uuid)[1]) + float(amount))
+            await ctx.reply(f'Updated user funds by: {amount} {DISPLAY_CURRENCY}')
 
     @commands.command(aliases=['cu'])
     async def createUser(self, ctx, uuid):
         # Force creates a user to have a balance
         if ctx.author.id == self.bot.owner_id:
-            """uuid = ''.join(uuid).strip('<@>')
-            ecDataManip.createUser(uuid)
-            await ctx.reply(f'Force created user balance.')"""
+            uuid = ''.join(uuid).strip('<@>')
+            self.bot.database.create_user(uuid)
+            await ctx.reply(f'Forced user balance creation.')
 
     @commands.command()
     async def kill(self, ctx):
         # Forces the bot to shut down, only use when a catastrophic bug arises!
         if ctx.author.id == self.bot.owner_id:
-            """await ctx.reply("**FORCE SHUTDOWN?**\nPlease say \'yes\' or \'y\' within 15 seconds to complete this action.")
+            await ctx.reply("**FORCE SHUTDOWN?**\nPlease say \'yes\' or \'y\' within 15 seconds to complete this action.")
             def check(m):
                 return (m.content.lower() == 'yes' or m.content.lower() == 'y') and m.channel == ctx.channel
             # Confirmation for the kill switch
-            try: msg = await client.wait_for('message', check=check, timeout=15.0)
+            try: msg = await self.bot.wait_for('message', check=check, timeout=15.0)
             except asyncio.TimeoutError: await ctx.reply("The action has been canceled.") # If timer runs out
             else: 
-                exit()"""
+                exit()
             
 async def setup(bot):
     await bot.add_cog(Admin(bot))

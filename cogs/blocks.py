cb0e3410ca49735017de975ff1662284d34a9abf
eldym <eldymoddys@gmail.com>
2026-03-01 21:28:24 -0500

    block command validates if user input is valid integer also updated block umbed to use snake case

--- cogs/blocks.py
+++ cogs/blocks.py
@@ -18,8 +18,9 @@ class Blocks(commands.Cog):
     @commands.cooldown(1, COOLDOWN, commands.BucketType.channel)
     async def block(self, ctx, block_number):
         """Gives block data given a block number."""
-        block = self.bot.database.get_block(block_number)
-        await ctx.reply(embed=self.block_embed(block))
+        try: block = self.bot.database.get_block(int(block_number))
+        except: await ctx.reply(embed=self.bot.error_embed("Invalid input for block number!"))
+        else: await ctx.reply(embed=self.block_embed(block))
 
     @commands.command(aliases=['cb', 'current', 'currentblock'])
     @commands.cooldown(1, COOLDOWN, commands.BucketType.channel)
@@ -28,17 +29,18 @@ class Blocks(commands.Cog):
         curr = self.bot.database.get_current_block()
         await ctx.reply(embed=self.block_embed(curr))
 
-    def block_embed(self, blockData):
+    def block_embed(self, block_data):
         # Generates block information embed if block exists
-        if blockData is not None:
-            embed=discord.Embed(title=f"Block #{blockData[0]} Information", description=f"{"*Current block!*" if blockData == self.bot.database.get_current_block() else ""}", color=EMB_COLOUR)
+        if block_data is not None:
+            # block data: block_data[0]: block number, block_data[1]: block reward, block_data[2]: difficulty, block_data[3]: difficulty threshold, block_data[4]: block creation time (in unix)
+            embed=discord.Embed(title=f"Block #{block_data[0]} Information", description=f"{"*Current block!*" if block_data == self.bot.database.get_current_block() else ""}", color=EMB_COLOUR)
             embed.set_thumbnail(url=self.emb_thumbnail_link)
-            embed.add_field(name="💵 Reward Amount", value=f"`{blockData[1]:.6f}` {self.display_currency}", inline=False)
-            embed.add_field(name="⚒️ Difficulty", value=f"`{blockData[2]}`", inline=False)
-            if blockData == self.bot.database.get_current_block(): embed.add_field(name="🌎 Current Pool Effort", value=f"{self.bot.database.get_pool_share_sum()[0]} Shares", inline=False)
-            embed.add_field(name="📊 Diff. Threshold", value=f"`{blockData[3]}`", inline=False)
-            embed.add_field(name="⌛ Block Creation Time", value=f"<t:{blockData[4]}:f>", inline=False)
-            embed.add_field(name="⏲️ Block Creation Time Unix", value=f"`{blockData[4]}`", inline=False)
+            embed.add_field(name="💵 Reward Amount", value=f"`{block_data[1]:.6f}` {self.display_currency}", inline=False)
+            embed.add_field(name="⚒️ Difficulty", value=f"`{block_data[2]}`", inline=False)
+            if block_data == self.bot.database.get_current_block(): embed.add_field(name="🌎 Current Pool Effort", value=f"{self.bot.database.get_pool_share_sum()[0]} Shares", inline=False)
+            embed.add_field(name="📊 Diff. Threshold", value=f"`{block_data[3]}`", inline=False)
+            embed.add_field(name="⌛ Block Creation Time", value=f"<t:{block_data[4]}:f>", inline=False)
+            embed.add_field(name="⏲️ Block Creation Time Unix", value=f"`{block_data[4]}`", inline=False)
             return embed
         else: return self.bot.error_embed("This block does not exist!") # Error embed if block doesn't exist
 
